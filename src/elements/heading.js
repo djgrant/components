e01923de7bdc2f6bf75a5efeca34a7e224732e04
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2020-07-21 21:10:32 +0100

    Page layout

--- src/elements/heading.js
+++ src/elements/heading.js
@@ -6,13 +6,15 @@ const getClassNames = (level) =>
     h1: tw("text-2xl", "font-bold"),
     h2: tw("text-xl", "text-gray-700"),
     h3: tw("text-lg", "font-bold"),
+    h4: tw("text-sm", "font-bold"),
+    h5: tw("text-xs", "font-medium"),
   }[level]);
 
-export const Heading = ({ children, as, ...levels }) => {
+export const Heading = ({ children, as, className, ...levels }) => {
   const level = Object.keys(levels).filter(Boolean)[0] || "h3";
   return React.createElement(
     as || level,
-    { className: getClassNames(level) },
+    { className: tw(getClassNames(level), className) },
     children
   );
 };
--- src/examples.js
+++ src/examples.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   Button,
   Card,
@@ -14,72 +13,80 @@ import {
 } from "./elements";
 
 export const Examples = () => (
-  <div className="flex p-4 bg-gray-200">
-    <div className="w-48 space-y-4">
-      <div className="text-xs font-bold text-gray-600 uppercase">
-        Components
+  <>
+    <header className="p-6 border-b border-gray-200">
+      <Heading h1 className="text-green-500">
+        @djgrant/components
+      </Heading>
+    </header>
+
+    <div className="flex">
+      <div className="w-48 p-6 space-y-4">
+        <Heading h5 className="text-gray-500 uppercase">
+          Components
+        </Heading>
+
+        <Tabs direction="vertical">
+          <Tab to="buttons" default>
+            Buttons
+          </Tab>
+          <Tab to="cards">Cards</Tab>
+          <Tab to="forms">Forms</Tab>
+          <Tab to="tabs">Tabs</Tab>
+          <Tab to="headings">Headings</Tab>
+        </Tabs>
       </div>
 
-      <Tabs direction="vertical">
-        <Tab to="buttons" default>
-          Buttons
-        </Tab>
-        <Tab to="cards">Cards</Tab>
-        <Tab to="forms">Forms</Tab>
-        <Tab to="tabs">Tabs</Tab>
-        <Tab to="headings">Headings</Tab>
-      </Tabs>
+      <Prose className="p-6">
+        <TabPage match="/buttons" default>
+          <h2>Buttons</h2>
+          <div className="flex space-x-2">
+            <Button>Click me</Button>
+            <Button type="primary">Click me</Button>
+            <Button type="blank">Click me</Button>
+          </div>
+        </TabPage>
+
+        <TabPage match="cards">
+          <h2>Cards</h2>
+          <Card>Hello card</Card>
+        </TabPage>
+
+        <TabPage match="forms">
+          <h2>Forms</h2>
+          <h3>Select</h3>
+          <Select>
+            <option>Option 1</option>
+            <option>Option 2</option>
+          </Select>
+        </TabPage>
+
+        <TabPage match="./tabs/*">
+          <h2>Tabs</h2>
+          <Tabs>
+            <Tab to="tabs/first" defaultOf="./tabs">
+              Hello
+            </Tab>
+            <Tab to="tabs/second">Hello</Tab>
+            <Tab to="tabs/third">Hello</Tab>
+          </Tabs>
+
+          <TabPanel match="tabs/first" defaultOf="./tabs">
+            First
+          </TabPanel>
+          <TabPanel match="tabs/second">
+            Second <Link to="/examples">Back to examples</Link>
+          </TabPanel>
+          <TabPanel match="tabs/third">Third</TabPanel>
+        </TabPage>
+
+        <TabPage match="headings">
+          <h2>Text</h2>
+          <Heading h1>Heading 1</Heading>
+          <Heading h2>Heading 2</Heading>
+          <Heading h3>Heading 3</Heading>
+        </TabPage>
+      </Prose>
     </div>
-
-    <Prose>
-      <TabPage match="/buttons" default>
-        <h2>Buttons</h2>
-        <div className="flex space-x-2">
-          <Button>Click me</Button>
-          <Button type="primary">Click me</Button>
-          <Button type="blank">Click me</Button>
-        </div>
-      </TabPage>
-
-      <TabPage match="cards">
-        <h2>Cards</h2>
-        <Card>Hello card</Card>
-      </TabPage>
-
-      <TabPage match="forms">
-        <h2>Forms</h2>
-        <h3>Select</h3>
-        <Select>
-          <option>Option 1</option>
-          <option>Option 2</option>
-        </Select>
-      </TabPage>
-
-      <TabPage match="./tabs/*">
-        <h2>Tabs</h2>
-        <Tabs>
-          <Tab to="tabs/first" defaultOf="./tabs">
-            Hello
-          </Tab>
-          <Tab to="tabs/second">Hello</Tab>
-          <Tab to="tabs/third">Hello</Tab>
-        </Tabs>
-
-        <TabPanel match="tabs/first" defaultOf="./tabs">
-          First
-        </TabPanel>
-        <TabPanel match="tabs/second">
-          Second <Link to="/examples">Back to examples</Link>
-        </TabPanel>
-        <TabPanel match="tabs/third">Third</TabPanel>
-      </TabPage>
-
-      <TabPage match="headings">
-        <h2>Text</h2>
-        <Heading h1>Heading 1</Heading>
-        <Heading h2>Heading 2</Heading>
-        <Heading h3>Heading 3</Heading>
-      </TabPage>
-    </Prose>
-  </div>
+  </>
 );
